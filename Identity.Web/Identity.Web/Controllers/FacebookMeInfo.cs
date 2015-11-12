e46f813cecf9fa96bda64c1fca5daa6738b5f8e1
nomiali5 <noomiali@yahoo.com>
2015-11-12 12:21:15 +0400

    FirstCommit

---
+++ Identity.Web/Identity.Web/Controllers/FacebookMeInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace Identity.Web.Controllers
+{
+    public class FacebookMeInfo
+    {
+        public FacebookMeInfo()
+        {
+        }
+
+        public dynamic Locale { get; set; }
+        public dynamic Name { get; set; }
+        public dynamic PictureUrl { get; set; }
+        public dynamic UpdatedTime { get; set; }
+    }
+}
